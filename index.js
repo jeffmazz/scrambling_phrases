d7b32b4273e3f7e9b00b9b27c95e9b6860703924
jeffmazz <jeff.mazzotta@hotmail.com>
2024-11-19 23:55:35 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const button = document.querySelector('.button')
+const scrambled_text = document.querySelector('.scrambled_text')
+
+const phrases = ["Hello, World!", "You're Welcome!", "Go Brazil!"]
+const characters = "abcdefghijklmnopqrstuvwxyz ,'"
+
+let selectedPhrase = []
+let scrambledArray = []
+
+const pickPhrase = () => {
+
+    selectedPhrase = []
+
+    let picked = phrases[Math.floor(Math.random()*phrases.length)]
+    let pickedArray = picked.split('')
+
+    pickedArray.forEach(char => {
+        selectedPhrase.push(char)
+    })
+
+}
+
+const handlePhrase = (array) => {
+
+    scrambled_text.innerHTML = ""
+
+    array.forEach(char => {
+        scrambled_text.innerHTML += char
+    })
+
+}
+
+const scrambleText = () => {
+
+    scrambledArray = []
+
+    selectedPhrase.forEach(el => {
+        scrambledArray.push(el.replace(el,characters[Math.floor(Math.random() * characters.length)]))
+    })
+    
+    console.log(scrambledArray)
+
+}
+
+button.addEventListener('click', () => {
+
+    button.disabled = true
+
+    pickPhrase()
+    scrambleText()
+
+    scrambledArray.forEach((el, index, array) => {
+
+        const delay = index * 200
+
+        setTimeout(() => {
+            let interval = setInterval(() => {
+                scrambledArray[index] = characters[Math.floor(Math.random()*characters.length)]
+                handlePhrase(scrambledArray)
+            }, 100)
+
+            setTimeout(() => {
+                clearInterval(interval)
+                scrambledArray[index] = selectedPhrase[index]
+                handlePhrase(scrambledArray)
+            }, 2000)
+
+        }, delay)
+
+    })
+
+    setTimeout(() => {
+        button.disabled = false
+    }, scrambledArray.length * 200 + 2000)
+
+})
